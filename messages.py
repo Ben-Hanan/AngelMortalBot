68f8bd0828c8e6def332a1fa0863030d80d6926b
Ben-Hanan <56751879+Ben-Hanan@users.noreply.github.com>
2021-06-23 13:29:46 +0800

    add missing part in start message

--- messages.py
+++ messages.py
@@ -15,7 +15,7 @@ START_MESSAGE = f'\n\nWelcome to Angel Mortal, a game where you try to be the be
 TUTORIAL = f'Since the aim of the game is to love on one another ' + '\u2764 ' + 'but remain anonymous ' + '\U0001F464' + ' at the same time, you can use me to relay your messages to both you angel and your mortal\! Isn\'t that convenient\!\n\n'
 EXAMPLES = f'Some examples of what you can do could be:\nSending them a prayer/words of encouragement\nyou can ask for their address and buy them food\!\nchat with them\n\n'
 END_START = f'If you need any help, you can bring up the help message through /help and if you\'re ready to start, you can find out who your mortal is by typing /revealmortal\n\n'
-EXTRA = f'to start chatting with either your angel or mortal, use the /setrecipient command to choose who to send your message to \n\nHave a blessed V Camp\!'
+EXTRA = f'To start chatting with either your angel or mortal, use the /setrecipient command to choose who to send your message to \n\nHave a blessed V Camp\!'
 
 HELP_MESSAGE = f'I am the middleman between you and your mortal or angel.\n\n' + f'You can control who you talk to using this command:\n\n'
 COMMAND = f'/setrecipient : choose to send messages to either your angel or mortal\n' + '/revealmortal : reveal your mortal to you!\n\n'
@@ -30,7 +30,7 @@ def format_angel_message(message):
     return 'From your angel:\n' + message
 
 def format_start_message():
-    return START_MESSAGE + TUTORIAL + EXAMPLES + END_START
+    return START_MESSAGE + TUTORIAL + EXAMPLES + END_START + EXTRA
 
 def format_help_message():
     return HELP_MESSAGE + COMMAND + LIMITATIONS + ASSISTANCE
