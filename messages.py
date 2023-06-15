d6338584e415f65f04b45da80757fc80d8f74790
Ben-Hanan <benhanan98@gmail.com>
2023-06-16 03:13:15 +0800

    edit text

--- messages.py
+++ messages.py
@@ -2,7 +2,7 @@ CONTACT = f'contact the admin @annoraaa'
 
 NOT_INITIALIZED = f'\n\nSorry\! But it looks like you are not in this round of angel mortal\! If this is a mistake please ' + CONTACT
 MESSAGE_SEND_FAIL = f'\n\nSorry! Your message failed to send, please ' + CONTACT
-BOT_NOT_STARTED = f'The player you are trying to chat with has not started this bot\. Please wait for them to start the bot\. If this is unexpected please ' + CONTACT
+BOT_NOT_STARTED = f'The player you are trying to chat with has not started this bot. Please wait for them to start the bot. If this is unexpected please ' + CONTACT
 MORTAL_NOT_FOUND = f"You don't have a mortal! If this is wrong, please " + CONTACT
 ALREADY_TEXTING_MORTAL = '\U0001F466\U0001F3FC' + f'You are chatting with your Mortal!' + '\U0001F467\U0001F3FC'
 ALREADY_TEXTING_ANGEL = '\U0001F47C\U0001F3FC' + f'You are chatting with your Angel!' + '\U0001F47C\U0001F3FC'
@@ -13,11 +13,11 @@ CHATTING_WITH_MORTAL= '\U0001F466\U0001F3FC' + " You are now chatting with your 
 
 START_MESSAGE = f'\n\nWelcome to Angel Mortal, a game where you try to be the best angel to your mortal by showering them with love ' + '\U0001F60D' + '\n\n'
 TUTORIAL = f'Since the aim of the game is to love on one another ' + '\u2764 ' + 'but remain anonymous ' + '\U0001F464' + ' at the same time, you can use me to relay your messages to both you angel and your mortal\! Isn\'t that convenient\!\n\n'
-EXAMPLES = f'Some examples of what you can do could be:\n\-sending them a prayer/words of encouragement\n\-you can buy them food\!\n\-chat with them\n\n'
+EXAMPLES = f'Some examples of what you can do would be:\n\- sending them a prayer/words of encouragement\n\- sending them some memes (only images though)\n\- you can get them food\!\n\- chat with them\n\n'
 END_START = f'If you need any help, you can bring up the help message through /help and if you\'re ready to start, you can find out who your mortal is by typing /revealmortal\n\n'
 EXTRA = f'To start chatting with either your angel or mortal, use the /setrecipient command to choose who to send your message to \n\nHave a blessed V Camp\!'
 
-HELP_MESSAGE = f'I am the middleman between you and your mortal or angel\.\n\n' + f'You can control who you talk to using this command:\n\n'
+HELP_MESSAGE = f'I am the middleman between you and your mortal or angel.\n\n' + f'You can control who you talk to using this command:\n\n'
 COMMAND = f'/setrecipient : choose to send messages to either your angel or mortal\n' + '/revealmortal : reveal your mortal to you!\n\n'
 LIMITATIONS = f'Keep in mind that in the current implementation of this bot, the only type of media I am able to relay are photos ' + '\U0001F62A' + '\n\n'
 ASSISTANCE = f'If you ever need more assistance, you can ' + CONTACT
